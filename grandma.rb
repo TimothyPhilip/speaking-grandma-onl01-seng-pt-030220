fbaa2774cf2ee1b0da60285b3e0205285a220c7f
Philippe Timothe <philippetimothe@gmail.com>
2020-03-06 20:57:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 
 def speak_to_grandma(say)
-  if say == "#{say}".upcase
-    return "NO, NOT SINCE 1938!"
-  elsif say == "I LOVE YOU GRANDMA!"
+  if say == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif say == "#{say}".upcase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
